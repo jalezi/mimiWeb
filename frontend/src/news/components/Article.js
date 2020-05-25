db6b10c86e5f84d4ff2ff5014b6f87ab67469a23
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-05-25 18:15:07 +0200

    tinyMCE successfully implemented

--- frontend/src/news/components/Article.js
+++ frontend/src/news/components/Article.js
@@ -1,10 +1,15 @@
 import React from 'react';
+var ReactDOMServer = require('react-dom/server');
+var HtmlToReactParser = require('html-to-react').Parser;
+var htmlToReactParser = new HtmlToReactParser();
 
 const Article = props => {
+  var reactElement = htmlToReactParser.parse(props.item.body);
+
   return (
-    <article>
+    <article id={props.item._id}>
       <h3>{props.item.title}</h3>
-      <p>{props.item.body}</p>
+      {reactElement}
     </article>
   );
 };
